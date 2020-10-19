b81a6973832a475b4f683de1870f1481f5169b44
Lucía Atienza Olmo <luciaatienzaolmo@MBP-de-Lucia.home>
2020-10-19 12:20:51 +0200

    algoritmos de ordenación

---
+++ 2/1.h
@@ -0,0 +1,8 @@
+#ifndef ORDENACION_H_
+#define ORDENACION_H_
+#include "1.cpp"
+void ordenacion_intercambio(int* p, int* f);
+void ordenacion_seleccion(int* p, int* f);
+void ordenacion_insercion(int* p, int* f);
+
+#endif
