8bd95aa0fac01cce1c9d09535c0071acfa68f7d1
hanon11 <lucia.atienza2@gmail.com>
2020-12-14 21:11:24 +0100

    práctica 4

--- 4/solitario.h
+++ 4/solitario.h
@@ -18,5 +18,16 @@ private:
     static const char* nom_palo[], * nom_fig[];
 
 };
+const char* tCarta::nom_palo[] = {"OROS   ", "COPAS  ", "ESPADAS", "BASTOS "};
+
+const char* tCarta::nom_fig[] = {"-------", "AS     ", "DOS    ", "TRES   ", 
+                                "CUATRO ", "CINCO  ", "SEIS   ", "SIETE  ",
+                                "SOTA   ", "CABALLO", "REY    "};
+
+std::ostream& operator <<(std::ostream& os, const tCarta& c)
+{
+    os << "(" << tCarta::nom_fig[c.verFig()] << ", " << tCarta::nom_palo[c.verPalo()] << ")";
+    return os;
+}
 
 #endif
