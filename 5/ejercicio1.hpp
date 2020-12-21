069c7140d9bd931f2df98cb4abb655224a65948c
hanon11 <lucia.atienza2@gmail.com>
2020-12-21 18:38:25 +0100

    práctica 5

---
+++ 5/ejercicio1.hpp
@@ -0,0 +1,26 @@
+#include "colaDina.h"
+#include "pila.h"
+
+template<typename T>
+bool isomorfos(const Pila<T>& pila, const Cola<T>& cola){
+    bool booleano = false;
+    if(!pila.vacia() && !cola.vacia()){
+        Pila<T> copiaP(pila);
+        Cola<T> copiaC(cola);
+    
+        copiaC.pop();
+        size_t contador = 1;
+        booleano = true;
+        while(!copiaP.vacia() && !copiaC.vacia() && booleano == true){
+            if(contador % 2 == 1)
+                booleano = booleano && copiaP.tope() == copiaC.frente();
+        
+            contador++;
+            copiaP.pop();
+            copiaC.pop();
+        }
+
+    }
+
+    return booleano;
+}
