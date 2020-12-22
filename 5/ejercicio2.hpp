8616e328a8f2b0afa2b7518c449a6c5b894ea359
hanon11 <lucia.atienza2@gmail.com>
2020-12-22 18:01:05 +0100

    práctica 5

---
+++ 5/ejercicio2.hpp
@@ -0,0 +1,38 @@
+#ifndef _EJERCICIO2_HPP
+#define _EJERCICIO2_HPP
+
+#include "colaDina.h"
+#include "pilaEnla.h"
+
+template<typename T>
+void invertir(Cola<T>& COLA,T a,T b){
+    if(!COLA.vacia()){
+        Cola<T> aux;
+        Pila<T> pila;
+
+        while(!COLA.vacia() && COLA.frente()!=a){
+            aux.push(COLA.frente());
+            COLA.pop();
+        }
+        
+        while(!COLA.vacia() && COLA.frente() != b){
+            pila.push(COLA.frente());
+            COLA.pop();
+        }
+
+        if(!COLA.vacia()){
+            while(!pila.vacia()){
+                aux.push(pila.tope());
+                pila.pop();
+            }
+            while(!COLA.vacia()){
+                aux.push(COLA.frente());
+                COLA.pop();
+            }
+
+            COLA = aux;
+            
+        }
+    }
+}
+#endif //_EJERCICIO2_HPP
