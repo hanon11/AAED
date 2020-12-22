fe73968e758f9eeed3d1447b50173d263abd44f1
hanon11 <lucia.atienza2@gmail.com>
2020-12-22 14:19:52 +0100

    práctica 5

--- 5/bicola.h
+++ 5/bicola.h
@@ -1,22 +1,17 @@
-#ifndef _bicola_H
-#define _bicola_H
+#ifndef _BICOLA_H
+#define _BICOLA_H
 #include <cassert>
 
-template <typename T>
-class bicola {
+template<typename T> 
+class Bicola{
 public:
-    bicola();
-    bicola(const bicola<T>& C);
-    bicola<T>& operator=(const bicola<T>& C);
-    bool vacia() const;
+    Bicola();
+    bool vacia()const;
     const T& frente() const;
+    const T& final() const;
     void popfrente();
     void pushfrente(const T& x);
-    const T& final() const;
-    void popfinal();
-    void pushfinal(const T& x);
-    ~bicola();
-
+    ~Bicola();
 private:
     struct nodo {
         T elem;
@@ -27,135 +22,75 @@ private:
         {
         }
     };
-
-    nodo* inicio;
     nodo* fin;
-    void copiar(const bicola<T>& C);
+    void copiar(const Bicola<T>& C);
 };
 
-template <typename T>
-inline bicola<T>::bicola()
-    : inicio(0)
-    , fin(0)
-{
-}
+template<typename T>
+inline Bicola<T>::Bicola():
+    fin(0)
+{}
+
 
 template <typename T>
-inline bicola<T>::bicola(const bicola<T>& C)
-    : inicio(0)
-    , fin(0)
-{
-    copiar(C);
-}
-
-template <typename T>
-bicola<T>& bicola<T>::operator=(const bicola<T>& C)
-{
-    if (this != &C) {
-        this->~bicola();
-        copiar(C);
-    }
-    return *this;
-}
-
-template <typename T>
-bool bicola<T>::vacia() const
-{
-    return (inicio == 0);
-}
-
-template <typename T>
-const T& bicola<T>::frente() const
-{
-    assert(!vacia());
-    return (inicio->elem);
-}
-
-template <typename T>
-void bicola<T>::popfrente()
-{
-    assert(!vacia());
-    nodo* p = inicio;
-    inicio = p->sig;
-    if (!inicio)
-        fin = 0;
-    delete p;
-}
-
-template <typename T>
-void bicola<T>::pushfrente(const T& x)
-{
-    nodo* p = new nodo(x);
-    if (inicio == 0) {
-        inicio = fin = p;
-    } else {
-        fin = fin->sig = p;
+void Bicola<T>::copiar(const Bicola<T>& B){
+    fin = new nodo(B.fin->elto);
+    nodo * q = B.fin->sig;
+    nodo * p = fin;
+    while(q!=B.fin)
+    {
+        p->sig=new nodo(q->elto);
+        p=p->sig;
+        q=q->sig;
     }
 }
 
-
-template <typename T>
-const T& bicola<T>::final() const
-{
-    assert(!vacia());
-    return (fin->elem);
+template<typename T>
+bool Bicola<T>::vacia()const{
+    return(fin == 0);
 }
 
-template <typename T>
-void bicola<T>::popfinal(){
+template<typename T>
+const T& Bicola<T>::frente()const{
     assert(!vacia());
-    nodo* p;
-    if (fin == fin->sig) {
-        p = fin;
-        fin = 0;
+    return(fin->sig->elem);
+}
+
+template<typename T>
+void Bicola<T>::popfrente(){
+    assert(!vacia());
+    nodo *p = fin->sig;
+    if(p == fin){
         delete p;
+        fin = fin->sig = 0;
     }else{
-        p = fin->sig;
-
-        while (p->sig != fin) {
-            p = p->sig;
-        }
-        fin = p;
-        //p->sig = fin->sig;
+        fin->sig = p->sig;
+        delete p;
     }
 }
 
-template <typename T>
-void bicola<T>::pushfinal(const T& x)
-{
-    nodo* p = new nodo(x);
-    if (fin == 0) {
+template<typename T>
+void Bicola<T>::pushfrente(const T& x){
+    nodo *p = new nodo(x);
+    if(fin == 0){
         fin = p->sig = p;
-    } else {
+    }else{
         p->sig = fin->sig;
         fin->sig = p;
-        fin = p;
     }
 }
 
-template <typename T>
-void bicola<T>::copiar(const bicola<T>& C)
-{
-    if (C.inicio) { // C no está vacía // Copiar el primer elto.
-        inicio = fin = new nodo(C.inicio->elem);
-        // Copiar el resto de elementos hasta el final de la bicola.
-        for (nodo* p = C.inicio->sig; p; p = p->sig) {
-            fin->sig = new nodo(p->elem);
-            fin = fin->sig;
-        }
-    }
-}
 
-template <typename T>
-bicola<T>::~bicola()
-{
+template<typename T>
+Bicola<T>::~Bicola(){
     nodo* p;
-    while (inicio) {
-        p = inicio->sig;
-        delete inicio;
-        inicio = p;
+    while (fin != fin->sig) {
+        p = fin->sig;
+        fin->sig = p->sig;
+        delete p;
     }
     fin = 0;
 }
 
-#endif // _bicola_h
+
+#endif // _BICOLA_H
